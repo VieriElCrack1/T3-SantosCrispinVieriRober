a2a3b52c20b6ce0a23f40fe321853bdabdf61661
VieriElCrack1 <santosvieri9@gmail.com>
2024-06-22 13:14:18 -0500

    pregunta 4 - commit - 1

--- src/app/dashboard/ejercicio3/ejercicio3.component.ts
+++ src/app/dashboard/ejercicio3/ejercicio3.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { ModuleModule } from '../../angular-module/module/module.module';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-ejercicio3',
   standalone: true,
-  imports: [],
+  imports: [ModuleModule, FormsModule],
   templateUrl: './ejercicio3.component.html',
   styleUrl: './ejercicio3.component.css'
 })
 export class Ejercicio3Component {
 
+  ejercicio3 = {
+    monto: null
+  }
+
+  resultado = "";
+
+  calcular() {
+    let monto = Number(this.ejercicio3.monto);
+    let interes = 0.0;
+    let valorTotalDeudaInteres = 0.0;
+    
+    
+  }
 }
