1cbad11ba7c08f66ba1210a65fbd91acac6babcb
VieriElCrack1 <santosvieri9@gmail.com>
2024-06-22 13:12:02 -0500

    pregunta 3 - commit - 2

--- src/app/dashboard/ejercicio2/ejercicio2.component.ts
+++ src/app/dashboard/ejercicio2/ejercicio2.component.ts
@@ -1,13 +1,51 @@
 import { Component } from '@angular/core';
 import { ModuleModule } from '../../angular-module/module/module.module';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-ejercicio2',
   standalone: true,
-  imports: [ModuleModule],
+  imports: [ModuleModule, FormsModule],
   templateUrl: './ejercicio2.component.html',
   styleUrl: './ejercicio2.component.css'
 })
 export class Ejercicio2Component {
 
+
+  resultado = "";
+
+  ejercicio2 = {
+    monto:null
+  }
+
+  calcular() {
+    let monto = Number(this.ejercicio2.monto);
+    let cuotas = 0;
+    let interes = 0.0;
+    let valorTotalDeuda = 0.0;
+    let valorTotal = 0.0;
+
+    if(monto < 1000) {
+      interes = 0.12;
+      cuotas = 1;
+    }else if(monto <= 3000) {
+      interes = 0.12;
+      cuotas = 2;
+    }else if (monto <= 3999){
+      interes = 0.12;
+      cuotas = 2;
+    }else {
+      interes = 0.10;
+      cuotas = 2;
+      if(monto > 5000) {
+        cuotas = 3;
+        interes = 0.10;
+      }
+    }
+
+    valorTotalDeuda = monto + (monto * interes);
+    valorTotal = valorTotalDeuda / cuotas;
+    
+    this.resultado = "Cantidad De Cuotas " + cuotas + ", Cuotas Por Pagar es : " + valorTotal;
+  }
 }
