96991167d5eb5c6726013b6cd465a9a4e7d15af4
VieriElCrack1 <santosvieri9@gmail.com>
2024-06-22 12:27:20 -0500

    pregunta 2 - commit - 1

--- src/app/dashboard/ejercicio1/ejercicio1.component.ts
+++ src/app/dashboard/ejercicio1/ejercicio1.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { ModuleModule } from '../../angular-module/module/module.module';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-ejercicio1',
   standalone: true,
-  imports: [],
+  imports: [ModuleModule, FormsModule],
   templateUrl: './ejercicio1.component.html',
   styleUrl: './ejercicio1.component.css'
 })
 export class Ejercicio1Component {
 
+  ejercicio1 = {
+    precio : null,
+    cantidad : null
+  }
+  
+  resultado = "";
+
+  calcular() {
+    let precio = Number(this.ejercicio1.precio);
+    let cantidad = Number(this.ejercicio1.cantidad);
+    let importePagar = 0.0;
+    let importeDescuento = 0.0;
+    if(precio > 200) {
+      importePagar = cantidad * precio;
+      importeDescuento = importePagar - (importePagar * 0.20);
+    }else {
+      importePagar = cantidad * precio;
+      importeDescuento = importePagar;
+    }
+    
+    this.resultado = "El Importe Total es: " + importeDescuento;
+  }
 }
--- src/app/dashboard/ejercicio2/ejercicio2.component.ts
+++ src/app/dashboard/ejercicio2/ejercicio2.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ModuleModule } from '../../angular-module/module/module.module';
 
 @Component({
   selector: 'app-ejercicio2',
   standalone: true,
-  imports: [],
+  imports: [ModuleModule],
   templateUrl: './ejercicio2.component.html',
   styleUrl: './ejercicio2.component.css'
 })
